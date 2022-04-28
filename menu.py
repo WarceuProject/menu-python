034af472885bd7cb3735693b1bc203e5516c7daf
lightid180@gmail.com <lightid180@gmail.com>
2022-04-28 21:29:48 +0700

    addcolor

--- menu.py
+++ menu.py
@@ -2,28 +2,33 @@
 # mari kita mulai membuat kerangka nya :v
 
 import os,sys
-
+# Oke,ini di part 2 nya ya ,kita tambahin sedikit warna biar cuantik :v
+class col:
+  red ="\033[1;31m"
+  grn ="\033[1;32m"
+  end ="\033[0m"
+  #Untuk warna agan bisa tambahin sesuka agan 
 def banner():
   os.system('clear')
   #ini untuk banner atau logo nya
-  print "   "
-  print "                Hallo gan"
-  print " Ini adalah program list opsi menu python2.7"
-  print "_____________________________________________"
+  print  "   "
+  print col.red + "                Hallo gan"
+  print col.grn + " Ini adalah program list opsi menu python2.7"
+  print col.end + "_____________________________________________"
   print ""
   # Untuk banner agan2 bisa sesuaikan 
 def menu():
   # Nah ini untuk menu nya
-  print "[MENU]:"
+  print col.end + "[" + col.grn + "MENU" + col.end + "]:"
   print ""
-  print "(1). Ospsi ke 1"
-  print "(2). Opsi ke 2 "
-  print "(0) Opsi keluar"
+  print col.end + "(1)." + col.grn + "Ospsi ke 1"
+  print col.end + "(2)." + col.grn + "Opsi ke 2 "
+  print col.end + "(0)." + col.grn + "Opsi keluar"
   
 #setelah itu kita sebut keduanya di bawah
 banner()
 menu()
-pil = input("pilih:")
+pil = input(col.end + "pilih:" + col.grn)
 if pil == 1:
   print "Ini adalah opsi pertama"
 elif pil == 2:
@@ -41,3 +46,7 @@ else:
   #NAH GAMPANG KAN :v
   #Oke segini dulu aja ya :v nanti lanjut di part 2 nya
   
+  
+  
+  #Nah sesudah itu agan bisa sesuai kan warna2 nya sesuka agan :v
+  # oke dah kita tinggal buat project nya aja deh terserah agan mo bikin apa juga :v
